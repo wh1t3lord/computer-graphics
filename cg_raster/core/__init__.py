855beb086f98606ac9a34260dd598b1f48739ff1
wh1t3lord <git.matvey@yandex.ru>
2025-11-24 21:54:04 +0300

    ADD: start camera thing;

--- cg_raster/core/__init__.py
+++ cg_raster/core/__init__.py
@@ -2,7 +2,7 @@ from enum import Enum
 import slangpy as spy
 from pathlib import Path
 
-from camera import Camera
+from core.camera import Camera
 
 class RenderingType(Enum):
     FORWARD = 0
--- cg_raster/core/camera.py
+++ cg_raster/core/camera.py
@@ -1,3 +1,46 @@
+import slangpy as spy
+import numpy as np
+
+# Euler based camera
 class Camera:
     def __init__():
         pass
+
+    # private
+
+    def _recompute():
+        pass
+
+    # public
+    def set_orientation():
+        pass
+
+    def set_position():
+        pass
+
+    def set_fov():
+        pass
+
+    def update():
+        pass
+
+    def get_matrix_view():
+        pass
+
+    def get_matrix_orientation():
+        pass
+
+    def get_position():
+        pass
+
+    def get_matrix_world():
+        pass
+
+    def get_fov():
+        pass
+
+# Quaternion based camera   
+class CameraQuat:
+    def __init__():
+        pass
+
