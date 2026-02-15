03c35c53661a05f91cff91105b77637dc67b6dae
wh1t3lord <git.matvey@yandex.ru>
2026-02-15 18:51:44 +0300

    ADD: device_type deduction based on events field content;

--- cg_raster/core/input.py
+++ cg_raster/core/input.py
@@ -1,7 +1,7 @@
 import slangpy as spy
 import configparser
 from dataclasses import dataclass
-from enum import Enum
+from enum import Enum, IntFlag, auto
 
 class eEventState(Enum):
     kPressed = 1
@@ -18,11 +18,18 @@ class eAxisType(Enum):
     kRelative = 2,
     kNone = -1
 
+class eDeviceType(IntFlag):
+    kMouse = auto(),
+    kKeyboard = auto(),
+    kJoy = auto(),
+    kNone = 0
+
 @dataclass
 class InputElementState:
     events : list
     value : spy.math.float1
     axis_type : eAxisType
+    device_type : eDeviceType
     state : eEventState
 
 
@@ -35,41 +42,66 @@ class Input:
                 events=[spy.KeyCode.w],
                 value=0.0,
                 axis_type=eAxisType.kAbsolute,
+                device_type=eDeviceType.kNone,
                 state=eEventState.kNone
             ),
             "MOVE_BACKWARD" : InputElementState(
                 events=[spy.KeyCode.s],
                 value=0.0,
                 axis_type=eAxisType.kAbsolute,
+                device_type=eDeviceType.kNone,
                 state=eEventState.kNone
             ),
             "MOVE_LEFT" : InputElementState(
                 events=[spy.KeyCode.a],
                 value=0.0,
                 axis_type=eAxisType.kAbsolute,
+                device_type=eDeviceType.kNone,
                 state=eEventState.kNone
             ),
             "MOVE_RIGHT" : InputElementState(
                 events=[spy.KeyCode.d],
                 value=0.0,
                 axis_type=eAxisType.kAbsolute,
+                device_type=eDeviceType.kNone,
                 state=eEventState.kNone
             ),
             'LOOK' : InputElementState(
                 events=[spy.MouseEventType.move],
                 value=0.0,
                 axis_type=eAxisType.kAbsolute,
+                device_type=eDeviceType.kNone,
                 state=eEventState.kNone
             ),
         }
+
+        self._init_bindings(self.bindings)
+
+        for bind_name, state in self.bindings.items():
+            assert state.device_type != eDeviceType.kNone, 'Failed to initialize your bindings due to incorrect device type deduction based on events fields'
     
+
+    def _init_bindings(self, bindings):
+        if bindings:
+            for bind_name, state in bindings.items():
+                if len(state.events) != 0:
+                    for event in state.events:
+                        if event:
+                            if type(event) is spy.KeyCode:
+                                state.device_type |= eDeviceType.kKeyboard
+
+                            if type(event) is spy.MouseEventType:
+                                state.device_type |= eDeviceType.kMouse
+
     def update_keyboard(
             self, 
             event : spy.KeyboardEvent
     ):
         if self.bindings:
             for bind_name, state in self.bindings.items():
-                pass
+                if event.key in state.events:
+                    pass
+
 
 
     def update_mouse(
